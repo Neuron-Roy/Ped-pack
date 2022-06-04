ee86ceb5ff82c22312eff825729016a0012b3114
Royzer <105925435+Neuron-Roy@users.noreply.github.com>
2022-06-05 04:23:37 +0530

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+files {
+	'data/**/*.meta'
+}
+
+author 'ROYZER#7999'
+description 'A single resource ped pack'
+
+
+data_file 'PED_METADATA_FILE' 'data/**/*.meta'
